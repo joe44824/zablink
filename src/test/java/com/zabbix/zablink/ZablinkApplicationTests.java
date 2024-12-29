4ae20817bc3567db322eccc2c742c7a3853e3af1
Habibi <habibi@habibis-mbp.lan>
2024-12-29 17:15:18 +0800

    removed content on log.txt

--- src/test/java/com/zabbix/zablink/ZablinkApplicationTests.java
+++ src/test/java/com/zabbix/zablink/ZablinkApplicationTests.java
@@ -1,13 +1,13 @@
-package com.zabbix.zablink;
+// package com.zabbix.zablink;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ZablinkApplicationTests {
+// @SpringBootTest
+// class ZablinkApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// @Test
+// void contextLoads() {
+// }
 
-}
+// }
