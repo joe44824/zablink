e5f592ef6dd6847353b2f834e6f792bdc5c52f4b
Habibi <habibi@habibis-mbp.lan>
2025-01-05 23:02:49 +0800

    ...

--- src/main/java/com/zabbix/zablink/model/Host.java
+++ src/main/java/com/zabbix/zablink/model/Host.java
@@ -1,21 +1,18 @@
 package com.zabbix.zablink.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Host {
     private String name;
     private String osVersion;
     private VMStatus status;
     private String ip;
     private Date createdTime;
-
-    public Host(String name, String osVersion, VMStatus status, String ip, Date createdTime) {
-        this.name = name;
-        this.osVersion = osVersion;
-        this.status = status;
-        this.ip = ip;
-        this.createdTime = createdTime;
-    }
-
-    // Getters and Setters
 }
