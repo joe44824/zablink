f5021c0a8169d9624c6072f082b8f98570460202
Habibi <habibi@habibis-mbp.lan>
2025-01-07 21:32:57 +0800

    ...

--- src/main/java/com/zabbix/zablink/controller/NetworkScanController.java
+++ src/main/java/com/zabbix/zablink/controller/NetworkScanController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -29,23 +31,20 @@ public class NetworkScanController {
     @GetMapping("/scan-network")
     public ResponseEntity<List<HostFact>> scanNetwork() {
         try {
-            // Map to store private IPs associated with live hosts
-            Map<String, String> hostToPrivateIpMap = new HashMap<>();
-
             // 1. Get IPs
+            List<String> allLiveHosts = new ArrayList<>();
             List<String> privateCIDRs = networkScanService.getPrivateCIDRs();
+
             for (String cidr : privateCIDRs) {
                 List<String> liveHosts = networkScanService.scanNetwork(cidr);
-                for (String host : liveHosts) {
-                    hostToPrivateIpMap.put(host, cidr);
-                }
+                allLiveHosts.addAll(liveHosts);
             }
 
             System.out.println("Captured IPs");
-            System.out.println(hostToPrivateIpMap);
+            System.out.println(allLiveHosts);
 
             // 2. Save IPs to INI file
-            networkScanService.saveToYamlFile(new ArrayList<>(hostToPrivateIpMap.keySet()));
+            networkScanService.saveToYamlFile(allLiveHosts);
 
             // 3. Execute Ansible playbook
             hostFactsPlaybookService.execute();
@@ -56,13 +55,11 @@ public class NetworkScanController {
             // 5. Map the host facts to the HostFact model
             List<HostFact> hostFacts = hostFactsData.stream().map(data -> {
                 HostFact fact = new HostFact();
-                String hostname = (String) data.get("hostname");
-
-                fact.setHostname(hostname);
+                fact.setHostname((String) data.get("hostname"));
                 fact.setUptimeSeconds((String) data.get("uptime_seconds"));
                 fact.setBiosDate((String) data.get("bios_date"));
                 fact.setOsVersion((String) data.get("os_version"));
-                fact.setIp(hostToPrivateIpMap.getOrDefault(hostname, "Unknown"));
+                fact.setIp((String) data.get("ip"));
                 return fact;
             }).collect(Collectors.toList());
 
